a42a90dadc210bca73a9e16887894d71ac9ca059
dereban25 <dereban25@gmail.com>
2018-08-02 19:58:30 +0300

    Only two lessons

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+
+
+enemy ={
+    'loc x': 70,
+    'loc y': 50,
+    'color': 'green',
+    'health': 100,
+    'name': 'Dereban',
+}
---
+++ Loops.py
@@ -0,0 +1,11 @@
+
+
+for x in range(100, 10, -2):
+    print(str(x)+' good day')
+    if x==50:
+        break
+print('------THE END-------')
+
+
+
+
