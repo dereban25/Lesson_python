6f82d2b2e7f8dae0e05efd1cacde2cb6782d1fdb
dereban25 <dereban25@gmail.com>
2018-08-02 20:14:11 +0300

    Update

--- Dictionary.py
+++ Dictionary.py
@@ -7,3 +7,20 @@ enemy ={
     'health': 100,
     'name': 'Dereban',
 }
+
+print('Location object in X position '+str(enemy['loc x']))
+print('Location object in Y position '+str(enemy['loc y']))
+enemy['rank'] = 'Admiral'
+print(enemy)
+del enemy['rank']
+print(enemy)
+
+enemy['loc x'] = enemy['loc x'] + 40
+enemy['health'] = enemy['health'] - 30
+if enemy['health'] < 80:
+    enemy['color'] = 'yellow'
+
+
+print(enemy)
+print(enemy.keys())
+print(enemy.values())
